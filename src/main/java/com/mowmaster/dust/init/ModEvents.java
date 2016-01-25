069179767bd839a2967786d4384ef4969060e12f
Mowmaster <mowmaster@outlook.com>
2016-01-25 14:38:40 -0600

    Pre Stream Push

--- src/main/java/com/mowmaster/dust/init/ModEvents.java
+++ src/main/java/com/mowmaster/dust/init/ModEvents.java
@@ -1,24 +1,6 @@
 package com.mowmaster.dust.init;
 
-import ibxm.Player;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.util.ChatComponentText;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
-import net.minecraftforge.event.entity.player.PlayerUseItemEvent;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
-/**
- * Created by KingMowmaster on 1/22/2016.
- */
 public class ModEvents {
 
 // many event priorities, Highest runs first, Lowest runs last,Normal is DEFAULT if none specified,this is a way to order events...
@@ -26,14 +8,6 @@ public class ModEvents {
    // public void onToolUse(ItemTooltipEvent event)
    // {
     // EntityPlayer.addPotionEffect((new PotionEffect(Potion.moveSlowdown.getId(),1,5)));
-   //     System.out.println("Test");
+   //System.out.println("Test");
    // }
-    @SubscribeEvent
-    public void onToolUse(PlayerUseItemEvent.Tick event)
-    {
-        if(event.item.getItem() == Items.bow) {
-
-            System.out.println("Test");
-        }
-    }
 }
