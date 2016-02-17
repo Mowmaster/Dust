88f38a3b2d846a3b741f38488bf9814482e72a1d
Mowmaster <mowmaster@outlook.com>
2016-02-17 16:04:23 -0600

    Added Localizations to creative tabs, bc it bugs me. Also precommit for build v0.2.2.1 (Alpha build)

--- src/main/java/com/mowmaster/dust/proxy/clientproxy.java
+++ src/main/java/com/mowmaster/dust/proxy/clientproxy.java
@@ -1,7 +1,11 @@
 package com.mowmaster.dust.proxy;
 
 
+import com.mowmaster.dust.block.BlockRenderRegister;
+import com.mowmaster.dust.item.ItemRenderRegister;
+
 public class ClientProxy extends CommonProxy
 {
 
 }
+
