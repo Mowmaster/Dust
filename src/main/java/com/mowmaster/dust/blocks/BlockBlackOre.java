6cbdedbb92e9497fc2ae30c436a2387ed65d70c7
Mowmaster <mowmaster@outlook.com>
2017-03-22 20:49:33 -0500

    Updated Black crystal, will do the other 6 in the morning

--- src/main/java/com/mowmaster/dust/blocks/BlockBlackOre.java
+++ src/main/java/com/mowmaster/dust/blocks/BlockBlackOre.java
@@ -2,49 +2,77 @@ package com.mowmaster.dust.blocks;
 
 import com.mowmaster.dust.blocks.item.IMetaBlockName;
 import com.mowmaster.dust.enums.CrystalBlocks;
+
 import com.mowmaster.dust.references.Reference;
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockProperties;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
 
+import static net.minecraft.block.BlockPistonBase.getFacingFromEntity;
+import static net.minecraft.util.BlockRenderLayer.CUTOUT;
+import static net.minecraft.util.BlockRenderLayer.SOLID;
 import static net.minecraft.util.BlockRenderLayer.TRANSLUCENT;
+import static net.minecraftforge.client.ForgeHooksClient.setRenderLayer;
 
 
-public class BlockBlackOre extends Block implements IMetaBlockName
+public class BlockBlackOre extends BlockDirectional implements IMetaBlockName
 {
+    //the string text is what you use in your json file
+    public static final PropertyEnum BLACKSTATE = PropertyEnum.create("BLACKSTATE",CrystalBlocks.CrystalOres.class);
 
-    public static final PropertyEnum BLACKSTATE = PropertyEnum.create("blackstate",CrystalBlocks.CrystalOres.class);
-
+	
+	
     public BlockBlackOre(String unloc)
     {
         super(Material.ROCK);
         this.setUnlocalizedName(unloc);
         this.setRegistryName(new ResourceLocation(Reference.MODID, unloc));
-        this.setDefaultState(this.blockState.getBaseState().withProperty(BLACKSTATE, CrystalBlocks.CrystalOres.ORE));
+        this.setDefaultState(this.blockState.getBaseState().withProperty(BLACKSTATE, CrystalBlocks.CrystalOres.ORE).withProperty(FACING, EnumFacing.UP));
     }
 
     @Override
     protected BlockStateContainer createBlockState()
     {
-        return new BlockStateContainer(this,new IProperty[]{BLACKSTATE});
+        return new BlockStateContainer(this,new IProperty[]{BLACKSTATE,FACING});
     }
 
+	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+
+	{
+		return super.onBlockPlaced(worldIn,pos, getFacingFromEntity(pos,placer),hitX,hitY,hitZ,meta,placer);
+	}
+	
+	public void onBlockPlacedBy(World worldIn,BlockPos pos,IBlockState state,EntityLivingBase placer,ItemStack stack)
+	{
+        worldIn.setBlockState(pos, state.withProperty(FACING, getFacingFromEntity(pos, placer)), 2);
+        EntityPlayer player = (EntityPlayer) placer;
+	}
+	
     @Override
     public int getMetaFromState(IBlockState state)
     {
@@ -55,8 +83,11 @@ public class BlockBlackOre extends Block implements IMetaBlockName
     @Override
     public IBlockState getStateFromMeta(int meta)
     {
-        return this.getDefaultState().withProperty(BLACKSTATE, CrystalBlocks.CrystalOres.values()[meta]);
+        //EnumFacing enumfacing = EnumFacing.UP;
+        //).withProperty(FACING, enumfacing
+        return this.getDefaultState().withProperty(BLACKSTATE,CrystalBlocks.CrystalOres.values()[meta]);
     }
+
 
     public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list)
     {
@@ -83,39 +114,68 @@ public class BlockBlackOre extends Block implements IMetaBlockName
         return getMetaFromState(state);
     }
 
-    @Override
-    public BlockRenderLayer getBlockLayer() {
-        return TRANSLUCENT;
-    }
 
-    //AxisAlignedBB(min x1, mix y1, min z1, max x2, max y2, max z2)
-    private static final AxisAlignedBB ORE_BOX = new AxisAlignedBB(
-            0.0D, 0.0D, 0.0D,
-            1.0D, 1.0D, 1.0D);
-    private static AxisAlignedBB CRYSTAL_BOX = new AxisAlignedBB(
-            0.25D, 0.0D, 0.25D,
-            0.75D, 0.90D, 0.75D);
-    private static final AxisAlignedBB BASE_BOX = new AxisAlignedBB(
-            0.25D, 0.0D, 0.25D,
-            0.75D, 0.125D, 0.75D);
-    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
-    {
-
-        switch ((CrystalBlocks.CrystalOres)state.getValue(BLACKSTATE))
+    public BlockRenderLayer getBlockLayer(IBlockState state) {
+        switch((CrystalBlocks.CrystalOres)state.getValue(BLACKSTATE))
         {
             case FIFTH:
             case FOURTH:
             case THIRD:
             case SECOND:
             case FIRST:
-                return CRYSTAL_BOX;
             case BASE:
-                return BASE_BOX;
+                return TRANSLUCENT;
+            case ORE:
+            default:
+                return SOLID ;
+        }
+
+    }
+
+    //AxisAlignedBB(min x1, mix y1, min z1, max x2, max y2, max z2)
+    private static AxisAlignedBB CDOWN = new AxisAlignedBB(0.25D, 1.0D, 0.25D, 0.75D, 0.1D, 0.75D);
+    private static AxisAlignedBB CUP = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.9D, 0.75D);
+    private static AxisAlignedBB CNORTH = new AxisAlignedBB(0.25D, 0.75D, 0.1D, 0.75D, 0.25D, 1.0D);
+    private static AxisAlignedBB CSOUTH = new AxisAlignedBB(0.25D, 0.75D, 0.0D, 0.75D, 0.25D, 0.9D);
+    private static AxisAlignedBB CWEST = new AxisAlignedBB(0.1D, 0.75D, 0.25D, 1.0D, 0.25D, 0.75D);
+    private static AxisAlignedBB CEAST = new AxisAlignedBB(0.0D, 0.75D, 0.25D, 0.9D, 0.25D, 0.75D);
+
+    private static final AxisAlignedBB BUP = new AxisAlignedBB(0.25D, 0.0D, 0.25D, 0.75D, 0.125D, 0.75D);
+    private static final AxisAlignedBB BDOWN = new AxisAlignedBB(0.25D, 0.875D, 0.25D, 0.75D, 1.0D, 0.75D);
+    private static final AxisAlignedBB BSOUTH = new AxisAlignedBB(0.25D, 0.75D, 0.125D, 0.75D, 0.25D, 0.0D);
+    private static final AxisAlignedBB BWEST = new AxisAlignedBB(0.875D, 0.75D, 0.25D, 1.0D, 0.25D, 0.75D);
+    private static final AxisAlignedBB BNORTH = new AxisAlignedBB(0.25D, 0.75D, 0.875D, 0.75D, 0.25D, 1.0D);
+    private static final AxisAlignedBB BEAST = new AxisAlignedBB(0.0D, 0.75D, 0.25D, 0.125D, 0.25D, 0.75D);
+
+    private static final AxisAlignedBB ORE_BOX = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
+
+
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos)
+    {
+        EnumFacing enumFacing = state.getValue(FACING);
+        switch ((CrystalBlocks.CrystalOres)state.getValue(BLACKSTATE)) {
+            case FIFTH:
+            case FOURTH:
+            case THIRD:
+            case SECOND:
+            case FIRST:
+                if (enumFacing == EnumFacing.UP) {return CUP;}
+                else if (enumFacing == EnumFacing.DOWN) {return CDOWN;}
+                else if(enumFacing == EnumFacing.NORTH) {return CNORTH;}
+                else if(enumFacing == EnumFacing.EAST) {return CEAST;}
+                else if(enumFacing == EnumFacing.SOUTH) {return CSOUTH;}
+                else if(enumFacing == EnumFacing.WEST)  {return CWEST;}
+            case BASE:
+                if (enumFacing == EnumFacing.UP) {return BUP;}
+                else if (enumFacing == EnumFacing.DOWN) {return BDOWN;}
+                else if(enumFacing == EnumFacing.NORTH) {return BNORTH;}
+                else if(enumFacing == EnumFacing.EAST) {return BEAST;}
+                else if(enumFacing == EnumFacing.SOUTH) {return BSOUTH;}
+                else if(enumFacing == EnumFacing.WEST) {return BWEST;}
             case ORE:
             default:
                 return ORE_BOX;
-        }
-
+            }
     }
     public boolean isOpaqueCube(IBlockState state)
     {
@@ -126,5 +186,7 @@ public class BlockBlackOre extends Block implements IMetaBlockName
     {
         return false;
     }
+
+
 
 }
