8649a5a7fb4f50c75febcbf99c4939c9bea53575
Mowmaster <mowmaster@outlook.com>
2017-03-23 07:17:16 -0500

    Working on World Generation for crystal ores

---
+++ src/main/java/com/mowmaster/dust/world/OreGeneration.java
@@ -0,0 +1,22 @@
+package com.mowmaster.dust.world;
+import java.util.*;
+
+public class OreGeneration implements IWorldGenerator
+{
+	//World Generators
+	private WorldGenerator overworld_red;
+	private WorldGenerator overworld_blue;
+	private WorldGenerator overworld_yellow;
+	private WorldGenerator overworld_purple;
+	private WorldGenerator overworld_orange;
+	private WorldGenerator overworld_green;
+	private WorldGenerator overworld_white;
+	private WorldGenerator overworld_black;
+
+
+	@Override
+	public void generate(Random random, int chunkZ,World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider)
+	{
+
+	}
+}
