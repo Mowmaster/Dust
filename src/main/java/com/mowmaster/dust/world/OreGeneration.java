d8e210c6945a1738009e5322b0a217dd80d0fea3
Mowmaster <mowmaster@outlook.com>
2017-07-28 15:50:58 -0500

    Changed ore gen per crystal biome temp, also pre 1.12 build commit

--- src/main/java/com/mowmaster/dust/world/OreGeneration.java
+++ src/main/java/com/mowmaster/dust/world/OreGeneration.java
@@ -116,9 +116,51 @@ public class OreGeneration implements IWorldGenerator
                     int x = chunk_X*16 + rand.nextInt(16);
                     int y = minHeight + rand.nextInt(heightDiff);
                     int z = chunk_Z*16 + rand.nextInt(16);
-                    if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_crystal) || world.getBiome(new BlockPos(x,y,z)).equals(crystal_hot) || world.getBiome(new BlockPos(x,y,z)).equals(crystal_warm) || world.getBiome(new BlockPos(x,y,z)).equals(crystal_cold))
+                    if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_crystal))
                     {generator.generate(world, rand, new BlockPos(x, y, z));}
                 }
+    }
+    private void runGeneratorHot(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        int heightDiff = maxHeight - minHeight + 1;
+        for (int i = 0; i < chancesToSpawn; i++) {
+            int x = chunk_X*16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunk_Z*16 + rand.nextInt(16);
+            if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_hot))
+            {generator.generate(world, rand, new BlockPos(x, y, z));}
+        }
+    }
+    private void runGeneratorWarm(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        int heightDiff = maxHeight - minHeight + 1;
+        for (int i = 0; i < chancesToSpawn; i++) {
+            int x = chunk_X*16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunk_Z*16 + rand.nextInt(16);
+            if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_warm))
+            {generator.generate(world, rand, new BlockPos(x, y, z));}
+        }
+    }
+    private void runGeneratorCold(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        int heightDiff = maxHeight - minHeight + 1;
+        for (int i = 0; i < chancesToSpawn; i++) {
+            int x = chunk_X*16 + rand.nextInt(16);
+            int y = minHeight + rand.nextInt(heightDiff);
+            int z = chunk_Z*16 + rand.nextInt(16);
+            if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_cold))
+            {generator.generate(world, rand, new BlockPos(x, y, z));}
+        }
     }
 
     private void runTreeGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
@@ -131,7 +173,53 @@ public class OreGeneration implements IWorldGenerator
                 int x = chunk_X*16 + rand.nextInt(9)+ 3;
                 int y = world.getHeight();
                 int z = chunk_Z*16 + rand.nextInt(9)+ 3;
-                if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_crystal) || world.getBiome(new BlockPos(x,y,z)).equals(crystal_hot) || world.getBiome(new BlockPos(x,y,z)).equals(crystal_warm) || world.getBiome(new BlockPos(x,y,z)).equals(crystal_cold)) {generator.generate(world, rand, world.getHeight(new BlockPos(x,y,z)));}
+                if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_crystal))
+                {generator.generate(world, rand, world.getHeight(new BlockPos(x,y,z)));}
+            }
+        }
+    }
+    private void runTreeGeneratorHot(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        if (rand.nextInt(randomnum) <= 1) {
+            for (int i = 0; i < chancesToSpawn; i++) {
+                int x = chunk_X*16 + rand.nextInt(9)+ 3;
+                int y = world.getHeight();
+                int z = chunk_Z*16 + rand.nextInt(9)+ 3;
+                if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_hot))
+                {generator.generate(world, rand, world.getHeight(new BlockPos(x,y,z)));}
+            }
+        }
+    }
+    private void runTreeGeneratorWarm(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        if (rand.nextInt(randomnum) <= 1) {
+            for (int i = 0; i < chancesToSpawn; i++) {
+                int x = chunk_X*16 + rand.nextInt(9)+ 3;
+                int y = world.getHeight();
+                int z = chunk_Z*16 + rand.nextInt(9)+ 3;
+                if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_warm))
+                {generator.generate(world, rand, world.getHeight(new BlockPos(x,y,z)));}
+            }
+        }
+    }
+    private void runTreeGeneratorCold(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z, int chancesToSpawn, int minHeight, int maxHeight) {
+
+        if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+            throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+        if (rand.nextInt(randomnum) <= 1) {
+            for (int i = 0; i < chancesToSpawn; i++) {
+                int x = chunk_X*16 + rand.nextInt(9)+ 3;
+                int y = world.getHeight();
+                int z = chunk_Z*16 + rand.nextInt(9)+ 3;
+                if(world.getBiome(new BlockPos(x,y,z)).equals(crystal_cold))
+                {generator.generate(world, rand, world.getHeight(new BlockPos(x,y,z)));}
             }
         }
     }
@@ -306,16 +394,32 @@ public class OreGeneration implements IWorldGenerator
                 this.runWorldGenerator(green_ore,world,random,chunkX,chunkZ,1,0,20);
                 this.runWorldGenerator(white_ore,world,random,chunkX,chunkZ,1,0,20);
                 this.runWorldGenerator(black_ore,world,random,chunkX,chunkZ,1,0,20);
-                //Ore Generator Crystal Biomes
-                this.runGenerator(red_ore,world,random,chunkX,chunkZ,5,0,60);
-                this.runGenerator(blue_ore,world,random,chunkX,chunkZ,5,0,60);
-                this.runGenerator(yellow_ore,world,random,chunkX,chunkZ,5,0,60);
-                this.runGenerator(purple_ore,world,random,chunkX,chunkZ,3,0,30);
-                this.runGenerator(orange_ore,world,random,chunkX,chunkZ,3,0,30);
-                this.runGenerator(green_ore,world,random,chunkX,chunkZ,3,0,30);
-                this.runGenerator(white_ore,world,random,chunkX,chunkZ,1,0,10);
-                this.runGenerator(black_ore,world,random,chunkX,chunkZ,1,0,10);
-                //Tree Generator Crystal Biomes
+                //Ore Generator Crystal Biome
+                this.runGenerator(red_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGenerator(blue_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGenerator(yellow_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGenerator(purple_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGenerator(orange_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGenerator(green_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGenerator(white_ore,world,random,chunkX,chunkZ,3,0,10);
+                this.runGenerator(black_ore,world,random,chunkX,chunkZ,3,0,10);
+                //Ore Generator Crystal Hot Biome
+                this.runGeneratorHot(red_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGeneratorHot(yellow_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGeneratorHot(orange_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGeneratorHot(white_ore,world,random,chunkX,chunkZ,3,0,10);
+                //Ore Generator Crystal Warm Biome
+                this.runGeneratorWarm(yellow_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGeneratorWarm(orange_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGeneratorWarm(green_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGeneratorWarm(white_ore,world,random,chunkX,chunkZ,3,0,10);
+                this.runGeneratorWarm(black_ore,world,random,chunkX,chunkZ,3,0,10);
+                //Ore Generator Crystal Cold Biome
+                this.runGeneratorCold(blue_ore,world,random,chunkX,chunkZ,15,0,60);
+                this.runGeneratorCold(purple_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGeneratorCold(green_ore,world,random,chunkX,chunkZ,9,0,30);
+                this.runGeneratorCold(black_ore,world,random,chunkX,chunkZ,3,0,10);
+                //Tree Generator Crystal Biome
                 this.runTreeGenerator(smalltreered, world, random, chunkX, chunkZ,1,0,20);
                 this.runTreeGenerator(smalltreeblue, world, random, chunkX, chunkZ,1,0,20);
                 this.runTreeGenerator(smalltreeyellow, world, random, chunkX, chunkZ,1,0,20);
@@ -324,6 +428,21 @@ public class OreGeneration implements IWorldGenerator
                 this.runTreeGenerator(smalltreegreen, world, random, chunkX, chunkZ,1,0,20);
                 this.runTreeGenerator(smalltreewhite, world, random, chunkX, chunkZ,1,0,20);
                 this.runTreeGenerator(smalltreeblack, world, random, chunkX, chunkZ,1,0,20);
+                //Tree Generator Crystal Hot Biome
+                this.runTreeGeneratorHot(smalltreered, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorHot(smalltreeyellow, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorHot(smalltreeorange, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorHot(smalltreewhite, world, random, chunkX, chunkZ,1,0,20);
+                //Tree Generator Crystal Warm Biome
+                this.runTreeGeneratorWarm(smalltreeyellow, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorWarm(smalltreeorange, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorWarm(smalltreegreen, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorWarm(smalltreewhite, world, random, chunkX, chunkZ,1,0,20);
+                //Tree Generator Crystal Cold Biome
+                this.runTreeGeneratorCold(smalltreeblue, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorCold(smalltreepurple, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorCold(smalltreegreen, world, random, chunkX, chunkZ,1,0,20);
+                this.runTreeGeneratorCold(smalltreeblack, world, random, chunkX, chunkZ,1,0,20);
                 //Overworld Structures All Biomes
                 this.runAllBiomeStructures(smallsandwell,world,random,chunkX,chunkZ,1,0,20);
                 this.runAllBiomeStructures(smallstonewell,world,random,chunkX,chunkZ,1,0,20);
