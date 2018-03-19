b48123b25afa8b268749ffcc0ead26372c0a3fb4
Mowmaster <mowmaster@outlook.com>
2018-03-19 17:43:33 -0500

    pre build testing and version increese

--- src/main/java/com/mowmaster/dust/references/Reference.java
+++ src/main/java/com/mowmaster/dust/references/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 {
     public static final String MODID = "dust";
     public static final String MODNAME = "Dust";
-    public static final String VERSION = "0.001.5";
+    public static final String VERSION = "0.001.6";
 
     public static final String CLIENT_SIDE = "com.mowmaster.dust.proxies.ClientProxy";
     public static final String SERVER_SIDE = "com.mowmaster.dust.proxies.CommonProxy";
