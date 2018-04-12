67fbae0046305aa2e2c0f9bce15b021dcd197f92
Mowmaster <mowmaster@outlook.com>
2018-04-12 17:16:27 -0500

    Enchantment Test for items

--- src/main/java/com/mowmaster/dust/handlers/DebugAndLogging.java
+++ src/main/java/com/mowmaster/dust/handlers/DebugAndLogging.java
@@ -3,12 +3,17 @@ package com.mowmaster.dust.handlers;
 import com.mowmaster.dust.blocks.BlockCrystalClusterBasic;
 import com.mowmaster.dust.blocks.BlockRegistry;
 import com.mowmaster.dust.items.ItemCrystal;
+import com.mowmaster.dust.items.ItemRegistry;
 import com.mowmaster.dust.tiles.TileCrystalCluster;
 import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentData;
+import net.minecraft.enchantment.EnchantmentKnockback;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.item.ItemEnchantedBook;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -22,6 +27,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import static com.mowmaster.dust.items.ItemRegistry.crystal;
 import static com.mowmaster.dust.items.ItemRegistry.debug;
@@ -36,25 +43,52 @@ public class DebugAndLogging
     {
         World worldIn = event.getWorld();
         EntityPlayer playerIn = event.getEntityPlayer();
+        BlockPos pos = playerIn.getPosition();
         EnumHand hand = event.getHand();
         IBlockState state = worldIn.getBlockState(event.getPos());
+        ItemStack item = playerIn.getHeldItem(EnumHand.MAIN_HAND);
 
         if((playerIn.getHeldItemOffhand().getItem().equals(debug)))
         {
-            if((playerIn.getHeldItemMainhand().isItemEnchanted() || playerIn.getHeldItemMainhand().getItem().equals(Items.ENCHANTED_BOOK)))
-            {
+            if((playerIn.getHeldItemMainhand().isItemEnchanted() || playerIn.getHeldItemMainhand().getItem().equals(Items.ENCHANTED_BOOK))) {
                 NBTTagList list = playerIn.getHeldItemMainhand().getEnchantmentTagList();
+                if (playerIn.getHeldItemMainhand().getItem().equals(Items.ENCHANTED_BOOK)) {
+                    list = ItemEnchantedBook.getEnchantments(item);
+                }
+
+
                 if (list == null) {
                     return;
                 }
 
+
+                int id = 0;
+                int lvl = 0;
+                Enchantment e = Enchantment.getEnchantmentByID(id);
                 for (int i = 0; i < list.tagCount(); i++) {
                     NBTTagCompound compound = list.getCompoundTagAt(i);
-                    int id = compound.getShort("id");
-                    int lvl = compound.getShort("lvl");
-                    Enchantment e = Enchantment.getEnchantmentByID(id);
+                    id = compound.getShort("id");
+                    lvl = compound.getShort("lvl");
+                    e = Enchantment.getEnchantmentByID(id);
 
-                    playerIn.sendMessage(new TextComponentString(TextFormatting.WHITE + e.getName() + ": " + lvl));
+                    if (e.getName().contains("enchantment.knockback"))
+                    {
+                        lvl = lvl+100;
+                        ItemStack stack = new ItemStack(item.getItem(), 1);
+                        stack.addEnchantment(Enchantment.getEnchantmentByID(id), lvl);
+
+                        if(!worldIn.isRemote)
+                        {
+                            worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5, pos.getY() + 0.0, pos.getZ() + 0.5, stack));
+                        }
+
+                    }
+
+                    if (!worldIn.isRemote) {
+                        playerIn.sendMessage(new TextComponentString(TextFormatting.WHITE + e.getName().replace("enchantment.", "") + ": " + TextFormatting.GOLD + lvl));
+                        playerIn.sendMessage(new TextComponentString(TextFormatting.WHITE + "ID" + ": " + TextFormatting.GREEN + id));
+                    }
+
                 }
             }
         }
