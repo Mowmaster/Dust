45dcdfe98a4d35f7e0725d99b68f4f575cff41ca
Mowmaster <mowmaster@outlook.com>
2018-05-30 15:47:20 -0500

    Starter Work on a Crystal Augmented Furnace

---
+++ src/main/java/com/mowmaster/dust/tiles/TileCrystalFurnace.java
@@ -0,0 +1,13 @@
+package com.mowmaster.dust.tiles;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ITickable;
+
+
+public class TileCrystalFurnace extends TileEntity //implements IInventory,ITickable
+{
+
+}
