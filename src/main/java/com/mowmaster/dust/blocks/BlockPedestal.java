99e3efa6ce26b4540d00896de424e9187e6a7e29
Mowmaster <mowmaster@outlook.com>
2018-05-29 20:36:27 -0500

    finished up coins recipes and made pedestals drop items properly

--- src/main/java/com/mowmaster/dust/blocks/BlockPedestal.java
+++ src/main/java/com/mowmaster/dust/blocks/BlockPedestal.java
@@ -9,12 +9,15 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -39,6 +42,34 @@ public class BlockPedestal extends Block implements ITileEntityProvider
         this.setCreativeTab(DUSTBLOCKSTABS);
         this.setSoundType(SoundType.STONE);
         this.setLightLevel(5f);
+    }
+
+    @Override
+    public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player) {
+        TileEntity tileEntity = worldIn.getTileEntity(pos);
+        if(tileEntity instanceof TilePedestal)
+        {
+            if(!worldIn.isRemote)
+            {
+                TilePedestal pedestal = (TilePedestal) tileEntity;
+                if(pedestal.hasItem()){worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,pedestal.getItemInPedestal()));}
+                if(pedestal.hasCoin()){worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,pedestal.getCoinOnPedestal()));}
+            }
+        }
+    }
+
+    @Override
+    public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state) {
+        TileEntity tileEntity = worldIn.getTileEntity(pos);
+        if(tileEntity instanceof TilePedestal)
+        {
+            if(!worldIn.isRemote)
+            {
+                TilePedestal pedestal = (TilePedestal) tileEntity;
+                if(pedestal.hasItem()){worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,pedestal.getItemInPedestal()));}
+                if(pedestal.hasCoin()){worldIn.spawnEntity(new EntityItem(worldIn, pos.getX() + 0.5,pos.getY() + 1.0,pos.getZ() + 0.5,pedestal.getCoinOnPedestal()));}
+            }
+        }
     }
 
     @Override
