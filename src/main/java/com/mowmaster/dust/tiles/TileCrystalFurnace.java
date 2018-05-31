63dfb39d82443be65af8acbf4c486f42503045cf
Mowmaster <mowmaster@outlook.com>
2018-05-30 19:19:32 -0500

    some tile getter work for future uses

--- src/main/java/com/mowmaster/dust/tiles/TileCrystalFurnace.java
+++ src/main/java/com/mowmaster/dust/tiles/TileCrystalFurnace.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.ITickable;
 
 
--- src/main/java/com/mowmaster/dust/tiles/TilePedestal.java
+++ src/main/java/com/mowmaster/dust/tiles/TilePedestal.java
@@ -1,15 +1,28 @@
 package com.mowmaster.dust.tiles;
 
 import com.mowmaster.dust.items.ItemCoin;
+import net.minecraft.block.BlockChest;
+import net.minecraft.block.BlockFurnace;
+import net.minecraft.block.BlockHopper;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.ITickable;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
 
-public class TilePedestal extends TileEntity
+import javax.annotation.Nonnull;
+
+public class TilePedestal extends TileEntity implements ITickable
 {
+
     private ItemStack item = ItemStack.EMPTY;
     private ItemStack coin = ItemStack.EMPTY;
 
@@ -76,6 +89,43 @@ public class TilePedestal extends TileEntity
         coin = ItemStack.EMPTY;
         updateBlock();
         return old;
+    }
+
+    private int ticker=0;
+    @Override
+    public void update() {
+        IBlockState state = world.getBlockState(pos.add(0,-1,0));
+        TileEntity tileEntity = world.getTileEntity(pos.add(0,-1,0));
+        if(!world.isRemote)
+        {
+            if(tileEntity instanceof TileEntityChest)
+            {
+                ticker++;
+                if(ticker>=20)
+                {
+                    System.out.println(((TileEntityChest) tileEntity).getSizeInventory());
+                    System.out.println(getNextSlotInChest(tileEntity));
+                    System.out.println(i);
+                    i=0;
+                    ticker=0;
+                }
+            }
+        }
+    }
+
+    private int i=0;
+    private ItemStack getNextSlotInChest(TileEntity entity)
+    {
+        int max=((TileEntityChest) entity).getSizeInventory();//27 for chests
+
+        for(i=0;i>((TileEntityChest) entity).getSizeInventory();i++)
+        {
+            if(!(((TileEntityChest) entity).getStackInSlot(i).equals(ItemStack.EMPTY)))
+            {
+                break;
+            }
+        }
+        return ((TileEntityChest) entity).getStackInSlot(i);
     }
 
     @Override
