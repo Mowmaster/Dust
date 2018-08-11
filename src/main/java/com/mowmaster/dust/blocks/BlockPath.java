7e72288470c1b50feb43092fd8b0902892faefd3
Mowmaster <mowmaster@outlook.com>
2018-08-11 14:26:11 -0500

    lots of texturing

--- src/main/java/com/mowmaster/dust/blocks/BlockPath.java
+++ src/main/java/com/mowmaster/dust/blocks/BlockPath.java
@@ -7,10 +7,13 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import java.util.Random;
 
 import static com.mowmaster.dust.misc.DustyTab.DUSTBLOCKSTABS;
@@ -37,6 +40,8 @@ public class BlockPath extends Block
     {
         return Item.getItemFromBlock(this);
     }
+
+
 
     @Override
     public void onEntityWalk(World worldIn, BlockPos pos, Entity entity) {
