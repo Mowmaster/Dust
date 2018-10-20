d723ba1cf87ccb3c4e379bb932516a9a7bbf7b14
Mowmaster <mowmaster@outlook.com>
2018-10-20 11:36:37 -0500

    fixed some instance issues and a tooltip issue

--- src/main/java/com/mowmaster/dust/items/ItemCrystalWrench.java
+++ src/main/java/com/mowmaster/dust/items/ItemCrystalWrench.java
@@ -3,30 +3,21 @@ package com.mowmaster.dust.items;
 import com.mowmaster.dust.blocks.BlockPedestal;
 import com.mowmaster.dust.references.Reference;
 import com.mowmaster.dust.tiles.TilePedestal;
-import net.minecraft.block.Block;
-import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.init.Enchantments;
-import net.minecraft.init.MobEffects;
-import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemSimpleFoiled;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -34,17 +25,19 @@ import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Random;
 
+import static com.mowmaster.dust.misc.DustyTab.DUSTTABS;
+
 
 public class ItemCrystalWrench extends Item
 {
-    BlockPos defaultPos = new BlockPos(0,-2000,0);
-    BlockPos storedPosition = defaultPos;
+    private static final BlockPos defaultPos = new BlockPos(0,-2000,0);
+    public BlockPos storedPosition = defaultPos;
     public ItemCrystalWrench(String unlocName, String registryName)
     {
         this.setUnlocalizedName(unlocName);
         this.setRegistryName(new ResourceLocation(Reference.MODID, registryName));
         this.maxStackSize = 1;
-        //this.setCreativeTab(DUSTTABS);
+        this.setCreativeTab(DUSTTABS);
     }
 
     int ticker=0;
@@ -52,7 +45,7 @@ public class ItemCrystalWrench extends Item
     public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
         if (stack.hasTagCompound()) {
             this.getPosFromNBT(stack);
-            BlockPos pos = this.getStoredPosition();
+            BlockPos pos = this.getStoredPosition(stack);
             Random rand = new Random();
 
             int zmin = -8;
@@ -65,24 +58,26 @@ public class ItemCrystalWrench extends Item
 
             if(storedPosition!=defaultPos)
             {
+
                 ticker++;
                 if(ticker>30)
                 {
-                    if(worldIn.isRemote)
-                    {
+                        //Test to see what location is stored in the wrench System.out.println(this.getStoredPosition(stack));
                         for (int c = zmin; c <= zmax; c++) {
                             for (int a = xmin; a <= xmax; a++) {
                                 for (int b = ymin; b <= ymax; b++) {
-
+                                    if(worldIn.isRemote)
+                                    {
                                     double d0 = (double) pos.getX() + 0.55D - (double) (rand.nextFloat() * 0.1F);
                                     double d1 = (double) pos.getY() + 0.55D - (double) (rand.nextFloat() * 0.1F);
                                     double d2 = (double) pos.getZ() + 0.55D - (double) (rand.nextFloat() * 0.1F);
                                     double d3 = (double) (0.4F - (rand.nextFloat() + rand.nextFloat()) * 0.4F);
                                     worldIn.spawnParticle(EnumParticleTypes.ENCHANTMENT_TABLE, pos.add(a,b,c).getX(),pos.add(a,b,c).getY(),pos.add(a,b,c).getZ(), rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D, rand.nextGaussian() * 0.005D, new int[0]);
+                                    }
                                 }
                             }
                         }
-                    }
+
                     ticker=0;
                 }
 
@@ -91,7 +86,9 @@ public class ItemCrystalWrench extends Item
         }
     }
 
-    public BlockPos getStoredPosition() {
+    public BlockPos getStoredPosition(ItemStack getWrenchItem)
+    {
+        getPosFromNBT(getWrenchItem);
         return storedPosition;
     }
 
@@ -107,39 +104,50 @@ public class ItemCrystalWrench extends Item
                 {
                     if(player.getHeldItem(hand).isItemEnchanted()==false)
                     {
-                        storedPosition = pos;
+                        //Gets Pedestal Clicked on Pos
+                        this.storedPosition = pos;
+                        //Writes to NBT
                         writePosToNBT(player.getHeldItem(hand));
+                        //Applies effect to wrench in hand
                         if(player.getHeldItem(hand).getItem() instanceof ItemCrystalWrench)
                         {
                             player.getHeldItem(hand).addEnchantment(Enchantments.UNBREAKING,-1);
                         }
                     }
+                    //If wrench has the compound stacks and has a position stored(is enchanted)
                     else if(player.getHeldItem(hand).hasTagCompound() && player.getHeldItem(hand).isItemEnchanted())
                     {
-                        TileEntity tileEntity = worldIn.getTileEntity(pos);
-                        if (tileEntity instanceof TilePedestal) {
-                            TilePedestal tilePedestal = (TilePedestal) tileEntity;
-                            if(tilePedestal.addOutputLocation(getStoredPosition()))
-                            {
-                                storedPosition = defaultPos;
-                                writePosToNBT(player.getHeldItem(hand));
-                                worldIn.notifyBlockUpdate(pos,worldIn.getBlockState(pos),worldIn.getBlockState(pos),2);
-                                if(player.getHeldItem(hand).getItem() instanceof ItemCrystalWrench)
+                        //Checks if clicked blocks is a Pedestal
+                        if(worldIn.getBlockState(pos).getBlock() instanceof BlockPedestal)
+                        {
+                            //Checks Tile at location to make sure its a TilePedestal
+                            TileEntity tileEntity = worldIn.getTileEntity(pos);
+                            if (tileEntity instanceof TilePedestal) {
+                                TilePedestal tilePedestal = (TilePedestal) tileEntity;
+                                //Checks if senderPedestal has locationSlots available
+                                if(tilePedestal.addOutputLocation(getStoredPosition(player.getHeldItem(hand))))
                                 {
-                                    if(player.getHeldItem(hand).isItemEnchanted())
+                                    //If slots are available then set wrench properties back to a default value
+                                    this.storedPosition = defaultPos;
+                                    writePosToNBT(player.getHeldItem(hand));
+                                    worldIn.notifyBlockUpdate(pos,worldIn.getBlockState(pos),worldIn.getBlockState(pos),2);
+                                    if(player.getHeldItem(hand).getItem() instanceof ItemCrystalWrench)
                                     {
-                                        player.getHeldItem(hand).removeSubCompound("ench");
+                                        if(player.getHeldItem(hand).isItemEnchanted())
+                                        {
+                                            player.getHeldItem(hand).removeSubCompound("ench");
+                                        }
                                     }
+                                    player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Link Successful"));
                                 }
-                                player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Link Successful"));
+                                else player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Link Unsuccessful"));
                             }
-                            else player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Link Unsuccessful"));
                         }
                     }
                 }
                 else
                 {
-                    storedPosition = defaultPos;
+                    this.storedPosition = defaultPos;
                     writePosToNBT(player.getHeldItem(hand));
                     worldIn.notifyBlockUpdate(pos,worldIn.getBlockState(pos),worldIn.getBlockState(pos),2);
                     if(player.getHeldItem(hand).getItem() instanceof ItemCrystalWrench)
@@ -159,9 +167,9 @@ public class ItemCrystalWrench extends Item
     public void writePosToNBT(ItemStack stack)
     {
         NBTTagCompound compound = new NBTTagCompound();
-        compound.setInteger("stored_x",storedPosition.getX());
-        compound.setInteger("stored_y",storedPosition.getY());
-        compound.setInteger("stored_z",storedPosition.getZ());
+        compound.setInteger("stored_x",this.storedPosition.getX());
+        compound.setInteger("stored_y",this.storedPosition.getY());
+        compound.setInteger("stored_z",this.storedPosition.getZ());
         stack.setTagCompound(compound);
     }
 
@@ -173,11 +181,12 @@ public class ItemCrystalWrench extends Item
             int x = getCompound.getInteger("stored_x");
             int y = getCompound.getInteger("stored_y");
             int z = getCompound.getInteger("stored_z");
-            storedPosition = new BlockPos(x,y,z);
+            this.storedPosition = new BlockPos(x,y,z);
         }
 
     }
 
+    /*
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
         ItemStack stack = playerIn.getHeldItem(handIn);
@@ -189,8 +198,7 @@ public class ItemCrystalWrench extends Item
         }
         return ActionResult.newResult(EnumActionResult.PASS,playerIn.getHeldItem(handIn));
     }
-
-
+     */
 
 
 
@@ -198,20 +206,13 @@ public class ItemCrystalWrench extends Item
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
 
-        if (stack.hasTagCompound())
-        {
-            if(stack.isItemEnchanted())
-            {
-                tooltip.add("Selected Block = X:" + storedPosition.getX() + " Y:" + storedPosition.getY() + " Z:" + storedPosition.getZ());
-            }
-            else tooltip.add("No Block Location Stored");
+        if(stack.getItem() instanceof ItemCrystalWrench) {
+            if (stack.hasTagCompound()) {
+                if (stack.isItemEnchanted()) {
+                    tooltip.add("Selected Block = X:" + this.getStoredPosition(stack).getX() + " Y:" + this.getStoredPosition(stack).getY() + " Z:" + this.getStoredPosition(stack).getZ());
+                } else tooltip.add("No Block Location Stored");
+            } else tooltip.add("No Block Location Stored");
         }
-        else tooltip.add("No Block Location Stored");
     }
 
-
-
-
-
 }
-
