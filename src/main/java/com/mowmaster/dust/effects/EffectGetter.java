2a8cae63cb55d554eaf9087be998c60cfeb14016
Mowmaster <mowmaster@outlook.com>
2018-11-01 20:57:35 -0500

    Updated Effect Picker(untested)

--- src/main/java/com/mowmaster/dust/effects/EffectGetter.java
+++ src/main/java/com/mowmaster/dust/effects/EffectGetter.java
@@ -3,6 +3,7 @@ package com.mowmaster.dust.effects;
 
 import com.google.common.collect.Maps;
 import net.minecraft.init.MobEffects;
+import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.fml.common.Loader;
 
@@ -22,38 +23,30 @@ public class EffectGetter
     private EffectGetter()
     {
         if(Loader.isModLoaded("immersiveengineering")) {immersiveE = true;}
-
+      //this.addSpellEffect(xRRRBBBYYY, new PotionEffect(MobEffects.STRENGTH));x=1 for good effect x=2 for bad effect otherwise its 0-100% of the mix is which of the three colors
         this.addSpellEffect(1100000000, new PotionEffect(MobEffects.STRENGTH));//100%red
-        this.addSpellEffect(1090000010, new PotionEffect(MobEffects.HEALTH_BOOST));//90% red 10% yellow
-        this.addSpellEffect(1080000020, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(1070000030, new PotionEffect(PotionRegistry.POTION_TILLER));
+        this.addSpellEffect(1099001000, new PotionEffect(MobEffects.INSTANT_HEALTH));
+        this.addSpellEffect(1090000010, new PotionEffect(MobEffects.HEALTH_BOOST));
+        this.addSpellEffect(1075000025, new PotionEffect(MobEffects.JUMP_BOOST));//red-orange
+        this.addSpellEffect(1075025000, new PotionEffect(PotionRegistry.POTION_MAGNETISM));//Red - Purple
         this.addSpellEffect(1060000040, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(1050000050, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(1040000060, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(1030000070, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(1020000080, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(1010000090, new PotionEffect(MobEffects.HEALTH_BOOST));//10%red 90%yellow
+        this.addSpellEffect(1050000050, new PotionEffect(MobEffects.HASTE));//orange 50/50
         this.addSpellEffect(1000000100, new PotionEffect(MobEffects.SATURATION));//100% yellow
-        this.addSpellEffect(1000010090, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000020080, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000030070, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000040060, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000050050, new PotionEffect(MobEffects.SATURATION));//
-        this.addSpellEffect(1000060040, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000070030, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000080020, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(1000090010, new PotionEffect(MobEffects.SATURATION));
+        this.addSpellEffect(1010000090, new PotionEffect(MobEffects.ABSORPTION));//100% yellow
+        this.addSpellEffect(1025000075, new PotionEffect(MobEffects.NIGHT_VISION));//yellow-orange
+        this.addSpellEffect(1000025075, new PotionEffect(MobEffects.INVISIBILITY));//green 50/50
+        this.addSpellEffect(1040000060, new PotionEffect(PotionRegistry.POTION_HARVESTER));
+        this.addSpellEffect(1000040060, new PotionEffect(PotionRegistry.POTION_GROWER));
+        this.addSpellEffect(1000050050, new PotionEffect(MobEffects.REGENERATION));//green 50/50
         this.addSpellEffect(1000100000, new PotionEffect(MobEffects.WATER_BREATHING));//100% Blue
-        this.addSpellEffect(1010090000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1020080000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1030070000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1040060000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1050050000, new PotionEffect(MobEffects.WATER_BREATHING));//
-        this.addSpellEffect(1060040000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1070030000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1080020000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(1090010000, new PotionEffect(MobEffects.WATER_BREATHING));
-        //this.addSpellEffect(1100000000, new PotionEffect(MobEffects.STRENGTH));//100%red
+        this.addSpellEffect(1000090010, new PotionEffect(MobEffects.SPEED));
+        this.addSpellEffect(1005095000, new PotionEffect(MobEffects.LUCK));
+        this.addSpellEffect(1000075025, new PotionEffect(PotionRegistry.POTION_QUICKNESS));//Blue - Green
+        this.addSpellEffect(1025075000, new PotionEffect(MobEffects.FIRE_RESISTANCE));//Blue - Purple
+        this.addSpellEffect(1000070030, new PotionEffect(PotionRegistry.POTION_STEPASSIST));
+        this.addSpellEffect(1000060040, new PotionEffect(PotionRegistry.POTION_PLANTER));
+        this.addSpellEffect(1040060000, new PotionEffect(PotionRegistry.POTION_WATERQUICKNESS));
+        this.addSpellEffect(1050050000, new PotionEffect(MobEffects.RESISTANCE));//purple 50/50
 
         this.addSpellEffect(1033033033, new PotionEffect(PotionRegistry.POTION_FLIGHT));//3 way split
         this.addSpellEffect(1025025050, new PotionEffect(PotionRegistry.POTION_SLOWFALL));
@@ -63,49 +56,34 @@ public class EffectGetter
 
 
 
-
-        this.addSpellEffect(2100000000, new PotionEffect(MobEffects.STRENGTH));//100%red
-        this.addSpellEffect(2090000010, new PotionEffect(MobEffects.HEALTH_BOOST));//90% red 10% yellow
-        this.addSpellEffect(2080000020, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2070000030, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2060000040, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2050000050, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2040000060, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2030000070, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2020000080, new PotionEffect(PotionRegistry.POTION_TILLER));
-        this.addSpellEffect(2010000090, new PotionEffect(MobEffects.HEALTH_BOOST));//10%red 90%yellow
-        this.addSpellEffect(2000000100, new PotionEffect(MobEffects.SATURATION));//100% yellow
-        this.addSpellEffect(2000010090, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000020080, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000030070, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000040060, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000050050, new PotionEffect(MobEffects.SATURATION));//
-        this.addSpellEffect(2000060040, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000070030, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000080020, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000090010, new PotionEffect(MobEffects.SATURATION));
-        this.addSpellEffect(2000100000, new PotionEffect(MobEffects.WATER_BREATHING));//100% Blue
-        this.addSpellEffect(2010090000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2020080000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2030070000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2040060000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2050050000, new PotionEffect(MobEffects.WATER_BREATHING));//
-        this.addSpellEffect(2060040000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2070030000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2080020000, new PotionEffect(MobEffects.WATER_BREATHING));
-        this.addSpellEffect(2090010000, new PotionEffect(MobEffects.WATER_BREATHING));
-        //this.addSpellEffect(2100000000, new PotionEffect(MobEffects.STRENGTH));//100%red
-
-        this.addSpellEffect(2033033033, new PotionEffect(PotionRegistry.POTION_FLIGHT));//3 way split
-        this.addSpellEffect(2025025050, new PotionEffect(PotionRegistry.POTION_SLOWFALL));
-        this.addSpellEffect(2025050025, new PotionEffect(PotionRegistry.POTION_SLOWFALL));
-        this.addSpellEffect(2050025025, new PotionEffect(PotionRegistry.POTION_SLOWFALL));
+        this.addSpellEffect(2100000000, new PotionEffect(MobEffects.WEAKNESS));//100%red
+        this.addSpellEffect(2099001000, new PotionEffect(MobEffects.INSTANT_DAMAGE));
+        this.addSpellEffect(2075000025, new PotionEffect(MobEffects.JUMP_BOOST,1,250));//red-orange
+        this.addSpellEffect(2050000050, new PotionEffect(MobEffects.MINING_FATIGUE));//orange 50/50
+        this.addSpellEffect(2000000100, new PotionEffect(MobEffects.HUNGER));//100% yellow
+        this.addSpellEffect(2025000075, new PotionEffect(MobEffects.BLINDNESS));//yellow-orange
+        this.addSpellEffect(2000025075, new PotionEffect(MobEffects.GLOWING));
+        this.addSpellEffect(2000050050, new PotionEffect(MobEffects.WITHER));//green 50/50
+        this.addSpellEffect(2000100000, new PotionEffect(PotionRegistry.POTION_DROWNING));//100% Blue
+        this.addSpellEffect(2000090010, new PotionEffect(MobEffects.SLOWNESS));
+        this.addSpellEffect(2005095000, new PotionEffect(MobEffects.UNLUCK));
+        if(immersiveE){this.addSpellEffect(2000075025, new PotionEffect(Potion.getPotionFromResourceLocation("immersiveengineering:slippery")));//Blue - Green
+            this.addSpellEffect(2025075000, new PotionEffect(Potion.getPotionFromResourceLocation("immersiveengineering:flammable")));//Blue - Purple
+            this.addSpellEffect(2040060000, new PotionEffect(Potion.getPotionFromResourceLocation("immersiveengineering:conductive")));
+            this.addSpellEffect(2000070030, new PotionEffect(Potion.getPotionFromResourceLocation("immersiveengineering:sticky")));
+            this.addSpellEffect(2000060040, new PotionEffect(Potion.getPotionFromResourceLocation("immersiveengineering:stunned")));
+            this.addSpellEffect(2000055045, new PotionEffect(Potion.getPotionFromResourceLocation("immersiveengineering:flashed")));}
+        this.addSpellEffect(2050050000, new PotionEffect(MobEffects.POISON));//purple 50/50
+        this.addSpellEffect(2033033033, new PotionEffect(MobEffects.LEVITATION));
+        this.addSpellEffect(2025025050, new PotionEffect(PotionRegistry.POTION_PETRIFIED));
+        this.addSpellEffect(2025050025, new PotionEffect(PotionRegistry.POTION_PETRIFIED));
+        this.addSpellEffect(2050025025, new PotionEffect(PotionRegistry.POTION_PETRIFIED));
     }
 
 
     public void addSpellEffect(int colorIn, PotionEffect effectOut)
     {
-        if(colorIn>=1000000000 && colorIn<=2100000000)
+        if(colorIn>=1000000000)
         {
             this.effectList.put(colorIn,new EffectHashMap(colorIn,effectOut));;
         }
