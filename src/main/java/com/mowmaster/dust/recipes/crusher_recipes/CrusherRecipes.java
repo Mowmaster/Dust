6c4a39106f6b5bd331272842a6c6152908cce27a
mowmaster <mowmaster@outlook.com>
2018-12-11 04:36:40 -0600

    Implimented a working crafting handler

--- src/main/java/com/mowmaster/dust/recipes/crusher_recipes/CrusherRecipes.java
+++ src/main/java/com/mowmaster/dust/recipes/crusher_recipes/CrusherRecipes.java
@@ -1,75 +1,105 @@
 package com.mowmaster.dust.recipes.crusher_recipes;
 
-
-import com.google.common.collect.Maps;
 import com.mowmaster.dust.blocks.BlockRegistry;
 import com.mowmaster.dust.items.ItemRegistry;
+import com.google.common.collect.Maps;
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
-import java.util.HashMap;
 import java.util.Map;
 
-import static com.google.common.collect.Maps.newHashMap;
 
+//Code directly taken from FurnaceRecipes
 public class CrusherRecipes
 {
     private static final CrusherRecipes CRUSHER_RECIPES = new CrusherRecipes();
-    private final HashMap<ItemStack,ItemStack> crusherList = new HashMap();
+    /** The list of smelting results. */
+    private final Map<ItemStack, ItemStack> crusherList = Maps.<ItemStack, ItemStack>newHashMap();
+
+    /**
+     * Returns an instance of FurnaceRecipes.
+     */
     public static CrusherRecipes instance()
     {
         return CRUSHER_RECIPES;
     }
 
-
     private CrusherRecipes()
     {
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,0),new ItemStack(BlockRegistry.redDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,1),new ItemStack(BlockRegistry.blueDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,2),new ItemStack(BlockRegistry.yellowDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,3),new ItemStack(BlockRegistry.purpleDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,4),new ItemStack(BlockRegistry.greenDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,5),new ItemStack(BlockRegistry.orangeDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,6),new ItemStack(BlockRegistry.whiteDust,8));
-        this.crusherList.put(new ItemStack(ItemRegistry.crystal,1,7),new ItemStack(BlockRegistry.blackDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,0),new ItemStack(BlockRegistry.redDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,1),new ItemStack(BlockRegistry.blueDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,2),new ItemStack(BlockRegistry.yellowDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,3),new ItemStack(BlockRegistry.purpleDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,4),new ItemStack(BlockRegistry.greenDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,5),new ItemStack(BlockRegistry.orangeDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,6),new ItemStack(BlockRegistry.whiteDust,8));
+        this.addCrusherRecipe(new ItemStack(ItemRegistry.crystal,1,7),new ItemStack(BlockRegistry.blackDust,8));
 
-        this.crusherList.put(new ItemStack(Blocks.REDSTONE_ORE,1),new ItemStack(BlockRegistry.redstoneDust,4));
-        this.crusherList.put(new ItemStack(Blocks.IRON_ORE,1),new ItemStack(BlockRegistry.ironDust,2));
-        this.crusherList.put(new ItemStack(Blocks.GOLD_ORE,1),new ItemStack(BlockRegistry.goldDust,2));
-        this.crusherList.put(new ItemStack(Blocks.REEDS,1),new ItemStack(BlockRegistry.sugarDust,2));
-        this.crusherList.put(new ItemStack(Items.WHEAT,1),new ItemStack(BlockRegistry.wheatDust,1));
-        this.crusherList.put(new ItemStack(Items.POTATO,1),new ItemStack(BlockRegistry.potatoDust,1));
-        this.crusherList.put(new ItemStack(Items.POISONOUS_POTATO,1),new ItemStack(BlockRegistry.potatoDust,1));
-        this.crusherList.put(new ItemStack(Items.BLAZE_ROD,1),new ItemStack(BlockRegistry.blazeDust,3));
+        this.addCrushingRecipeForBlock(Blocks.REDSTONE_ORE,new ItemStack(BlockRegistry.redstoneDust,4));
+        this.addCrushingRecipeForBlock(Blocks.IRON_ORE,new ItemStack(BlockRegistry.ironDust,2));
+        this.addCrushingRecipeForBlock(Blocks.GOLD_ORE,new ItemStack(BlockRegistry.goldDust,2));
+        this.addCrushing(Items.REEDS,new ItemStack(BlockRegistry.sugarDust,2));
+        this.addCrushing(Items.WHEAT,new ItemStack(BlockRegistry.wheatDust,1));
+        this.addCrushing(Items.POTATO,new ItemStack(BlockRegistry.potatoDust,1));
+        this.addCrushing(Items.POISONOUS_POTATO,new ItemStack(BlockRegistry.potatoDust,1));
+        this.addCrushing(Items.BLAZE_ROD,new ItemStack(BlockRegistry.blazeDust,3));
     }
 
-
-    public void addCrusherRecipe(ItemStack inputItem, ItemStack outputBlock)
+    /**
+     * Adds a smelting recipe, where the input item is an instance of Block.
+     */
+    public void addCrushingRecipeForBlock(Block input, ItemStack stack)
     {
-        this.crusherList.put(inputItem,outputBlock);
+        this.addCrushing(Item.getItemFromBlock(input), stack);
     }
 
-    public boolean hasOutput(ItemStack inputItem)
+    /**
+     * Adds a smelting recipe using an Item as the input item.
+     */
+    public void addCrushing(Item input, ItemStack stack)
     {
-        /*
-        if(!crusherList.get(inputItem).isEmpty())
+        this.addCrusherRecipe(new ItemStack(input, 1, 32767), stack);
+    }
+
+    /**
+     * Adds a smelting recipe using an ItemStack as the input for the recipe.
+     */
+    public void addCrusherRecipe(ItemStack input, ItemStack stack)
+    {
+        if (getCrushingResult(input) != ItemStack.EMPTY) { net.minecraftforge.fml.common.FMLLog.log.info("Ignored crystal crushing recipe with conflicting input: {} = {}", input, stack); return; }
+        this.crusherList.put(input, stack);
+    }
+
+    /**
+     * Returns the smelting result of an item.
+     */
+    public ItemStack getCrushingResult(ItemStack stack)
+    {
+        for (Map.Entry<ItemStack, ItemStack> entry : this.crusherList.entrySet())
         {
-            return true;
-        }
-         */
-        System.out.println(crusherList.get(inputItem).getDisplayName());
-
-        return false;
-    }
-
-    public ItemStack getOutput(ItemStack inputItem)
-    {
-        if(hasOutput(inputItem))
-        {
-            return crusherList.get(inputItem);
+            if (this.compareItemStacks(stack, entry.getKey()))
+            {
+                return entry.getValue();
+            }
         }
 
         return ItemStack.EMPTY;
     }
+
+    /**
+     * Compares two itemstacks to ensure that they are the same. This checks both the item and the metadata of the item.
+     */
+    private boolean compareItemStacks(ItemStack stack1, ItemStack stack2)
+    {
+        return stack2.getItem() == stack1.getItem() && (stack2.getMetadata() == 32767 || stack2.getMetadata() == stack1.getMetadata());
+    }
+
+    public Map<ItemStack, ItemStack> getCrushingList()
+    {
+        return this.crusherList;
+    }
+
 }
