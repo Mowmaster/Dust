0f11cd0c5dbc04277f7cea063ecd244ae7696204
Mowmaster <mowmaster@outlook.com>
2019-02-15 16:19:02 -0600

    first port to 1.13

--- src/main/java/com/mowmaster/dust/dust.java
+++ src/main/java/com/mowmaster/dust/dust.java
@@ -1,3 +1,4 @@
+/*
 package com.mowmaster.dust;
 
 
@@ -6,30 +7,14 @@ import com.mowmaster.dust.effects.PotionRegistry;
 import com.mowmaster.dust.enchantments.EnchantmentRegistry;
 import com.mowmaster.dust.items.ItemArmorAndToolsRegistry;
 import com.mowmaster.dust.items.ItemRegistry;
-import com.mowmaster.dust.misc.AchievementHandler;
 import com.mowmaster.dust.misc.DustConfigurationFile;
-import com.mowmaster.dust.proxies.ClientProxy;
 import com.mowmaster.dust.recipes.*;
 import com.mowmaster.dust.recipes.fuels.FuelRegistry;
-import com.mowmaster.dust.tiles.TileRegistry;
 import com.mowmaster.dust.world.biome.BiomeRegistry;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootTable;
-import net.minecraftforge.common.ForgeHooks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.util.RecipeMatcher;
-import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import com.mowmaster.dust.proxies.CommonProxy;
 import com.mowmaster.dust.references.Reference;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
-import net.minecraftforge.oredict.RecipeSorter;
-import net.minecraftforge.registries.ForgeRegistry;
 
 import java.io.File;
 
@@ -83,5 +68,89 @@ public class dust {
         CraftingRecipes.CraftingRecipes();
     }
 
+}
+ */
+package com.mowmaster.dust;
+
+        import net.minecraft.block.Block;
+        import net.minecraft.init.Blocks;
+        import net.minecraftforge.common.MinecraftForge;
+        import net.minecraftforge.event.RegistryEvent;
+        import net.minecraftforge.eventbus.api.SubscribeEvent;
+        import net.minecraftforge.fml.InterModComms;
+        import net.minecraftforge.fml.common.Mod;
+        import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+        import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+        import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+        import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+        import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+        import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+        import org.apache.logging.log4j.LogManager;
+        import org.apache.logging.log4j.Logger;
+
+        import java.util.stream.Collectors;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod("examplemod")
+public class dust
+{
+    // Directly reference a log4j logger.
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public dust() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        // Register the enqueueIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        // Register the processIMC method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        // Register the doClientStuff method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        // Register ourselves for server, registry and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+        // do something that can only be done on the client
+        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().gameSettings);
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+        // some example code to dispatch IMC to another mod
+        InterModComms.sendTo("forge", "helloworld", () -> { LOGGER.info("Hello world"); return "Hello world";});
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+        // some example code to receive and process InterModComms from other mods
+        LOGGER.info("Got IMC", event.getIMCStream().
+                map(m->m.getMessageSupplier().get()).
+                collect(Collectors.toList()));
+    }
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public static void onServerStarting(FMLServerStartingEvent event) {
+        // do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
+
+    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD event bus
+    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    public static class RegistryEvents {
+        @SubscribeEvent
+        public void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
+            // register a new block here
+            LOGGER.info("HELLO from Register Block");
+        }
+    }
 }
 
