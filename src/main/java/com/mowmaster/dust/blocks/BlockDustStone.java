f1da2dce7f283cd2fead9b5a7b3ba66c4948f58f
Mowmaster <mowmaster@outlook.com>
2019-02-17 00:21:00 -0600

    just testing some things out

--- src/main/java/com/mowmaster/dust/blocks/BlockDustStone.java
+++ src/main/java/com/mowmaster/dust/blocks/BlockDustStone.java
@@ -1,9 +1,57 @@
 package com.mowmaster.dust.blocks;
 
+import com.mowmaster.dust.references.Reference;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IItemProvider;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 
 public class BlockDustStone extends Block
 {
+    public BlockDustStone(Properties builder) {
+        super(builder);
+    }
+
+    public IItemProvider getItemDropped(IBlockState state, World worldIn, BlockPos pos, int fortune) {
+        return this;
+    }
+
+    private static final ResourceLocation RES_STONE_RED = new ResourceLocation(Reference.MODID, "stonered");
+
+    @SubscribeEvent
+    public static void onItemRegistryReady(RegistryEvent.Register<Item> event)
+    {
+        event.getRegistry().register(STONE_RED_ITEM);
+    }
+
+    @SubscribeEvent
+    public static void onBlockRegistryReady(RegistryEvent.Register<Block> event)
+    {
+        event.getRegistry().register(STONE_RED);
+    }
+
+    public static final Block STONE_RED = new Block(Block.Properties.create(Material.ROCK, MaterialColor.BLACK)
+            .hardnessAndResistance(5.0F, 10.0F)
+            .sound(SoundType.STONE))
+            .setRegistryName(RES_STONE_RED);
+
+    public static final Item STONE_RED_ITEM = new ItemBlock(STONE_RED, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
+    {
+
+
+    }.setRegistryName(RES_STONE_RED);
+
 
 }
--- src/main/java/com/mowmaster/dust/dust.java
+++ src/main/java/com/mowmaster/dust/dust.java
@@ -1,15 +1,9 @@
 package com.mowmaster.dust;
 
+import com.mowmaster.dust.blocks.BlockDustStone;
 import com.mowmaster.dust.references.Reference;
 import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,32 +14,15 @@ import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 @Mod(Reference.MODID)
 public class dust
 {
-    private static final ResourceLocation RES_STONE_RED = new ResourceLocation(Reference.MODID, "stonered");
-
     @SubscribeEvent
     public static void onItemRegistryReady(RegistryEvent.Register<Item> event)
     {
-        event.getRegistry().register(STONE_RED_ITEM);
+        BlockDustStone.onItemRegistryReady(event);
     }
 
     @SubscribeEvent
     public static void onBlockRegistryReady(RegistryEvent.Register<Block> event)
     {
-        event.getRegistry().register(STONE_RED);
+        BlockDustStone.onBlockRegistryReady(event);
     }
-
-    public static final Block STONE_RED = new Block(Block.Properties.create(Material.ROCK, MaterialColor.BLACK)
-            .hardnessAndResistance(5.0F, 10.0F)
-            .sound(SoundType.STONE))
-            .setRegistryName(RES_STONE_RED);
-
-    public static final Item STONE_RED_ITEM = new ItemBlock(STONE_RED, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS))
-    {
-        @Override
-        public int getBurnTime(ItemStack itemBlock)
-        {
-            return 16000;
-        }
-
-    }.setRegistryName(RES_STONE_RED);
 }
