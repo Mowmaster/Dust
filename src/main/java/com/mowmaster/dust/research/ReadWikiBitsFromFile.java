0653744e3af11645d4913625b44ea05f3d8309ea
mowmaster <mowmaster@outlook.com>
2019-11-27 16:32:06 -0600

    attempt to start parsing json files

---
+++ src/main/java/com/mowmaster/dust/research/ReadWikiBitsFromFile.java
@@ -0,0 +1,22 @@
+package com.mowmaster.dust.research;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ReadWikiBitsFromFile
+{
+    //ResourceLocation loc = new ResourceLocation("modid:researchnotes/blockpedestal.json");
+    //File filey = Minecraft.getMinecraft().getResourceManager().getResource(loc);
+
+
+}
