5748008ac795bd47a8ea0b1e1575e68049c7379a
Mowmaster <mowmaster@outlook.com>
2020-02-03 22:36:35 -0600

    update alpha build number

--- src/main/java/com/mowmaster/dust/references/Reference.java
+++ src/main/java/com/mowmaster/dust/references/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 {
     public static final String MODID = "dust";
     public static final String MODNAME = "Dust";
-    public static final String VERSION = "0.003.6";
+    public static final String VERSION = "0.003.6a";
 
     public static final String CLIENT_SIDE = "com.mowmaster.dust.proxies.ClientProxy";
     public static final String SERVER_SIDE = "com.mowmaster.dust.proxies.CommonProxy";
