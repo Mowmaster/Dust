8bd233fd578f2972a11c8070ff7c5223be8711b2
Mowmaster <mowmaster@outlook.com>
2020-02-29 11:11:12 -0600

    Fixed issue with upgrades effecting the wrong area of blocks

--- src/main/java/com/mowmaster/dust/items/itemPedestalUpgrades/ipuBasic.java
+++ src/main/java/com/mowmaster/dust/items/itemPedestalUpgrades/ipuBasic.java
@@ -444,7 +444,9 @@ public class ipuBasic extends Item
         }
     }
 
-    /*public BlockPos getNegRangePos(World world,BlockPos posOfPedestal, int intWidth, int intHeight)
+    
+
+    public BlockPos getNegRangePos(World world,BlockPos posOfPedestal, int intWidth, int intHeight)
     {
         IBlockState state = world.getBlockState(posOfPedestal);
         EnumFacing enumfacing = state.getValue(FACING);
@@ -487,54 +489,6 @@ public class ipuBasic extends Item
                 return blockBelow.add(intHeight,intWidth,intWidth);
             case WEST:
                 return blockBelow.add(0,intWidth,intWidth);
-            default:
-                return blockBelow;
-        }
-    }*/
-
-    public BlockPos getNegRangePos(World world,BlockPos posOfPedestal, int intWidth, int intHeight)
-    {
-        IBlockState state = world.getBlockState(posOfPedestal);
-        EnumFacing enumfacing = state.getValue(FACING);
-        BlockPos blockBelow = posOfPedestal;
-        switch (enumfacing)
-        {
-            case UP:
-                return blockBelow.add(-intWidth,0,-intWidth);
-            case DOWN:
-                return blockBelow.add(-intWidth,-intHeight,-intWidth);
-            case NORTH:
-                return blockBelow.add(-intWidth,-intWidth,-intHeight);
-            case SOUTH:
-                return blockBelow.add(-intWidth,-intWidth,0);
-            case EAST:
-                return blockBelow.add(0,-intWidth,-intWidth);
-            case WEST:
-                return blockBelow.add(-intHeight,-intWidth,-intWidth);
-            default:
-                return blockBelow;
-        }
-    }
-
-    public BlockPos getPosRangePos(World world,BlockPos posOfPedestal, int intWidth, int intHeight)
-    {
-        IBlockState state = world.getBlockState(posOfPedestal);
-        EnumFacing enumfacing = state.getValue(FACING);
-        BlockPos blockBelow = posOfPedestal;
-        switch (enumfacing)
-        {
-            case UP:
-                return blockBelow.add(intWidth+1,intHeight,intWidth+1);
-            case DOWN:
-                return blockBelow.add(intWidth+1,0,intWidth+1);
-            case NORTH:
-                return blockBelow.add(intWidth+1,intWidth,0+1);
-            case SOUTH:
-                return blockBelow.add(intWidth+1,intWidth,intHeight+1);
-            case EAST:
-                return blockBelow.add(intHeight+1,intWidth,intWidth+1);
-            case WEST:
-                return blockBelow.add(0+1,intWidth,intWidth+1);
             default:
                 return blockBelow;
         }
