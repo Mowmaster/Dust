d384ab60f6ad0a98fdda8941a000247f7b5cc2da
Mowmaster <mowmaster@outlook.com>
2021-12-06 08:09:01 -0600

    made a ToolTIpUtils class to help streamline tooltips a bit

--- src/main/java/com/mowmaster/dust/Items/Tools/BaseTool.java
+++ src/main/java/com/mowmaster/dust/Items/Tools/BaseTool.java
@@ -5,7 +5,9 @@ import com.mowmaster.dust.Block.Pedestal.BasePedestalBlock;
 import com.mowmaster.dust.DeferredRegistery.DeferredRegisterItems;
 import com.mowmaster.dust.Networking.DustPacketHandler;
 import com.mowmaster.dust.Networking.DustPacketParticles;
+import com.mowmaster.dust.Util.TooltipUtils;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
@@ -22,6 +24,7 @@ import javax.annotation.Nullable;
 import java.util.List;
 
 import static com.mowmaster.dust.Block.Pedestal.BasePedestalBlock.FACING;
+import static com.mowmaster.dust.References.Constants.MODID;
 
 public class BaseTool extends Item
 {
@@ -97,12 +100,8 @@ public class BaseTool extends Item
     public void appendHoverText(ItemStack p_41421_, @Nullable Level p_41422_, List<Component> p_41423_, TooltipFlag p_41424_) {
         super.appendHoverText(p_41421_, p_41422_, p_41423_, p_41424_);
 
-        TranslatableComponent base = new TranslatableComponent(getDescriptionId() + ".description");
-        base.withStyle(ChatFormatting.LIGHT_PURPLE);
-        p_41423_.add(base);
+        TooltipUtils.addTooltipShiftMessageWithStyle(p_41423_,getDescriptionId() + ".description",ChatFormatting.LIGHT_PURPLE);
+        TooltipUtils.addTooltipAltMessageWithStyle(p_41423_,getDescriptionId() + ".description_use",ChatFormatting.AQUA);
 
-        TranslatableComponent use = new TranslatableComponent(getDescriptionId() + ".description_use");
-        use.withStyle(ChatFormatting.AQUA);
-        p_41423_.add(use);
     }
 }
---
+++ src/main/java/com/mowmaster/dust/Util/TooltipUtils.java
@@ -0,0 +1,104 @@
+package com.mowmaster.dust.Util;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+import static com.mowmaster.dust.References.Constants.MODID;
+
+//https://www.youtube.com/watch?v=uOAeHJtZMZM
+public class TooltipUtils
+{
+
+    public static void addTooltipShiftMessage(List<Component> componentList, ItemStack stack, TranslatableComponent translatableComponent)
+    {
+        if(!Screen.hasShiftDown())
+        {
+            TranslatableComponent base = new TranslatableComponent(MODID + ".description_shift");
+            base.withStyle(ChatFormatting.WHITE);
+            componentList.add(base);
+        }
+        else {
+            TranslatableComponent base = translatableComponent;
+            componentList.add(base);
+        }
+    }
+
+    public static void addTooltipShiftMessageWithStyle(List<Component> componentList, String localizationString, ChatFormatting chatFormatting)
+    {
+        if(!Screen.hasShiftDown())
+        {
+            TranslatableComponent base = new TranslatableComponent(MODID + ".description_shift");
+            base.withStyle(ChatFormatting.WHITE);
+            componentList.add(base);
+        }
+        else {
+            TranslatableComponent base = new TranslatableComponent(localizationString);
+            base.withStyle(chatFormatting);
+            componentList.add(base);
+        }
+    }
+
+    public static void addTooltipShiftMessageWithStyle(List<Component> componentList, TranslatableComponent translatableComponent, ChatFormatting chatFormatting)
+    {
+        if(!Screen.hasShiftDown())
+        {
+            TranslatableComponent base = new TranslatableComponent(MODID + ".description_shift");
+            base.withStyle(ChatFormatting.WHITE);
+            componentList.add(base);
+        }
+        else {
+            TranslatableComponent base = translatableComponent;
+            base.withStyle(chatFormatting);
+            componentList.add(base);
+        }
+    }
+
+    public static void addTooltipAltMessage(List<Component> componentList, ItemStack stack, TranslatableComponent translatableComponent)
+    {
+        if(!Screen.hasAltDown())
+        {
+            TranslatableComponent base = new TranslatableComponent(MODID + ".description_alt");
+            base.withStyle(ChatFormatting.WHITE);
+            componentList.add(base);
+        }
+        else {
+            TranslatableComponent base = translatableComponent;
+            componentList.add(base);
+        }
+    }
+
+    public static void addTooltipAltMessageWithStyle(List<Component> componentList, String localizationString, ChatFormatting chatFormatting)
+    {
+        if(!Screen.hasAltDown())
+        {
+            TranslatableComponent base = new TranslatableComponent(MODID + ".description_alt");
+            base.withStyle(ChatFormatting.WHITE);
+            componentList.add(base);
+        }
+        else {
+            TranslatableComponent base = new TranslatableComponent(localizationString);
+            base.withStyle(chatFormatting);
+            componentList.add(base);
+        }
+    }
+
+    public static void addTooltipAltMessageWithStyle(List<Component> componentList, TranslatableComponent translatableComponent, ChatFormatting chatFormatting)
+    {
+        if(!Screen.hasAltDown())
+        {
+            TranslatableComponent base = new TranslatableComponent(MODID + ".description_alt");
+            base.withStyle(ChatFormatting.WHITE);
+            componentList.add(base);
+        }
+        else {
+            TranslatableComponent base = translatableComponent;
+            base.withStyle(chatFormatting);
+            componentList.add(base);
+        }
+    }
+}
